d58fddd96a90c2e90d5c6377506b894fcffadba9
VadimStarton <sedoigamer@gmail.com>
2024-01-20 17:50:09 +0200

    testas

--- App/DB/DataBase.php
+++ App/DB/DataBase.php
@@ -4,15 +4,10 @@ namespace App\DB;
 
 interface DataBase
 {
-
     function create(object $userData) : int;
-    
     function update(int $userId, object $userData) : bool;
-    
     function delete(int $userId) : bool;
-    
     function show(int $userId) : object;
-    
-    function showAll() : array;
+        function showAll() : array;
     
 }
--- App/DB/FileBase.php
+++ App/DB/FileBase.php
@@ -2,48 +2,54 @@
 
 namespace App\DB;
 
-// use App\DB\DataBase;
+//use App\DB\DataBase;
 
 class FileBase implements DataBase
 {
-    private $file, $data, $index, $save = true;
+    private $indexFile, $file, $data, $index, $save = true;
 
     public function __construct($name)
     {
         $this->file = ROOT . 'data/' . $name . '.json';
         $this->indexFile = ROOT . 'data/' . $name . '-index.json';
-        if (!file_exists($this->file)) {
-            file_put_contents($this->file, json_encode([]));
-            file_put_contents($this->indexFile, json_encode(1));
-        }
-        $this->data = json_decode(file_get_contents($this->file));
-        $this->index = json_decode(file_get_contents($this->indexFile));
+        //sitas ifas ivyksta jei kazka naujo padarom
+        if (!file_exists($this->file)) { //tikrinam ar failas egsituoja. tik pirma karta tikrinama
+            file_put_contents($this->file, json_encode([])); //jei nera, sukuria tuscius
+            file_put_contents($this->indexFile, json_encode(1)); //pradinis indeksas yra vienas.
+        } // kai failai sugeneruoti, toliau vyksta nuskaitymas
+        $this->data = json_decode(file_get_contents($this->file)); //nuskaitomas masyvas
+        $this->index = json_decode(file_get_contents($this->indexFile));// nuskaitomas indexas
     }
 
     public function __destruct()
     {
         if ($this->save) {
+            //paima is data ir index duomenis ir iraso juos i failus. Kad jis nepasileistu darant show ir show all uzdedam flaga savinimo.
             file_put_contents($this->file, json_encode($this->data));
             file_put_contents($this->indexFile, json_encode($this->index));
         }
     }   
 
-
-    public function create(object $data) : int
+    public function create(object $data) : int //situos data gaunam is creato
     {
-        $id = $this->index;
-        $this->index++;
-        $data->id = $id;
-        $this->data[] = $data;
-        return $id;
+        $this->save = true;
+        
+        $id = $this->index; //pasiima indexa
+        $this->index++; //paruosia sekanti indexa
+        $data->id = $id; //i data idedam idx
+        $this->data[] = $data; //ir viska ipaiso i duomenis
+        return $id; //grazina naujai sukurto id, jo nenaudojam, bet gaunam, OK.
     }
 
-    public function update(int $id, object $data) : bool
+
+    public function update(int $id, object $userData) : bool
     {
+        $this->save = true;
+        
         foreach ($this->data as $key => $value) {
-            if ($value->id == $id) {
-                $data->id = $id;
-                $this->data[$key] = $data;
+            if ($value->id === $id) {
+
+                $this->data[$key] = $userData;
                 return true;
             }
         }
@@ -52,19 +58,21 @@ class FileBase implements DataBase
 
     public function delete(int $id) : bool
     {
+        $this->save = true;
         foreach ($this->data as $key => $value) {
             if ($value->id == $id) {
-                unset($this->data[$key]);
-                $this->data = array_values($this->data);
+                unset($this->data[$key]);//po istrynimo atsiranda skyle, po json iraso kaip masyva, su skyle negali buti, todel pries tai dar array_values padarom
+                $this->data = array_values($this->data); //sugrazina i teisingas eiles.
                 return true;
             }
         }
         return false;
     }
 
-    public function show(int $id) : object
+    public function show(int $id) : object 
     {
-        $this->save = false;
+       
+        $this->save = false; //jei tik show, seivint nereikia, pazymim kad false
         foreach ($this->data as $key => $value) {
             if ($value->id == $id) {
                 return $value;
@@ -76,8 +84,10 @@ class FileBase implements DataBase
     public function showAll() : array
     {
         $this->save = false;
-        return $this->data;
+        return $this->data; //grazina pries tai konstruktoriuje nuskaitytus duomenis, visa masyva
     }
 
+
+    
 
 }
